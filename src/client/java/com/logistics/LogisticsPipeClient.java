81db3edd9143f905124038234525ac37483f6e8c
Kyle Klaus <kklaus@indemnity83.com>
2026-02-17 14:08:59 -0800

    fix: item tinting on marking fluid items

--- src/client/java/com/logistics/LogisticsPipeClient.java
+++ src/client/java/com/logistics/LogisticsPipeClient.java
@@ -2,7 +2,12 @@ package com.logistics;
 
 import com.logistics.core.bootstrap.DomainBootstrap;
 import com.logistics.pipe.screen.ItemFilterScreen;
+import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
 import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.item.Item;
+
+import java.util.stream.Stream;
 
 import static com.logistics.LogisticsMod.LOGGER;
 
@@ -30,6 +35,25 @@ public final class LogisticsPipeClient implements DomainBootstrap {
         // TODO: Restore pipe block entity renderer once rendering is ported to MC 1.21.1
         // BlockEntityRenderers.register(LogisticsPipe.ENTITY.PIPE_BLOCK_ENTITY, PipeBlockEntityRenderer::new);
 
+        registerMarkingFluidColors();
+
         MenuScreens.register(LogisticsPipe.SCREEN.ITEM_FILTER, ItemFilterScreen::new);
+    }
+
+    /**
+     * Tints layer1 (overlay) of the marking fluid item with the corresponding dye color.
+     * layer0 (the bottle/container) is left untinted.
+     */
+    private void registerMarkingFluidColors() {
+        Item[] items = Stream.of(DyeColor.values())
+                .map(LogisticsPipe::getMarkingFluidItem)
+                .toArray(Item[]::new);
+
+        ColorProviderRegistry.ITEM.register((stack, tintIndex) -> {
+            if (tintIndex == 0) return -1;
+
+            DyeColor color = LogisticsPipe.getMarkingFluidColor(stack);
+            return color != null ? (color.getFireworkColor() | 0xFF000000) : -1;
+        }, items);
     }
 }
