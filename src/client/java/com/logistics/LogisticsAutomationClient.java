105defad83b30b65b574220b9798c17d3958b6bb
Kyle Klaus <kklaus@indemnity83.com>
2026-02-18 20:15:44 -0800

    fix: restore laser quarry block entity renderer and client render cache cleanup (#127)

--- src/client/java/com/logistics/LogisticsAutomationClient.java
+++ src/client/java/com/logistics/LogisticsAutomationClient.java
@@ -1,14 +1,31 @@
 package com.logistics;
 
+import com.logistics.automation.render.ClientRenderCacheHooks;
+import com.logistics.automation.render.LaserQuarryBlockEntityRenderer;
 import com.logistics.core.bootstrap.DomainBootstrap;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererRegistry;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
 
 import static com.logistics.LogisticsMod.LOGGER;
 
 public final class LogisticsAutomationClient implements DomainBootstrap {
     public LogisticsAutomationClient() {
-        // Public constructor for direct instantiation
+        ModelLoadingPlugin.register(pluginContext -> {
+            ResourceLocation arm = LogisticsAutomation.blockModelIdentifier("laser_quarry_gantry_arm");
+            ResourceLocation drill = LogisticsAutomation.blockModelIdentifier("laser_quarry_drill");
+            ResourceLocation ledGreen = LogisticsAutomation.blockModelIdentifier("laser_quarry_led_green");
+            ResourceLocation ledRed = LogisticsAutomation.blockModelIdentifier("laser_quarry_led_red");
+            ResourceLocation display = LogisticsAutomation.blockModelIdentifier("laser_quarry_display");
+            ResourceLocation topHatch = LogisticsAutomation.blockModelIdentifier("laser_quarry_top_hatch");
+
+            pluginContext.addModels(arm, drill, ledGreen, ledRed, display, topHatch);
+        });
     }
 
     @Override
@@ -19,25 +36,25 @@ public final class LogisticsAutomationClient implements DomainBootstrap {
     @Override
     public void initClient() {
         LOGGER.info("Registering automation (client)");
-        // Register quarry frame for cutout rendering (transparency support)
+        // Register quarry blocks for cutout rendering (transparency support)
+        BlockRenderLayerMap.INSTANCE.putBlock(LogisticsAutomation.BLOCK.LASER_QUARRY, RenderType.cutout());
         BlockRenderLayerMap.INSTANCE.putBlock(LogisticsAutomation.BLOCK.LASER_QUARRY_FRAME, RenderType.cutout());
 
-        // TODO: Restore laser quarry block entity renderer once rendering is ported to MC 1.21.1
-        // BlockEntityRendererRegistry.register(
-        //         LogisticsAutomation.ENTITY.LASER_QUARRY_BLOCK_ENTITY, LaserQuarryBlockEntityRenderer::new);
+        BlockEntityRendererRegistry.register(
+                LogisticsAutomation.ENTITY.LASER_QUARRY_BLOCK_ENTITY, LaserQuarryBlockEntityRenderer::new);
 
         // No screen handler for laser quarry (no GUI)
 
-        // TODO: Restore render cache cleanup callbacks
-        // ClientRenderCacheHooks.setQuarryInterpolationClearer(LaserQuarryRenderState::clearInterpolationCache);
-        // ClientRenderCacheHooks.setClearAllInterpolationCaches(LaserQuarryRenderState::clearAllInterpolationCaches);
-        // ClientTickEvents.END_CLIENT_TICK.register(client -> {
-        //     if (client.level != null) {
-        //         LaserQuarryRenderState.pruneInterpolationCache(client.level);
-        //     }
-        // });
-        // ClientPlayConnectionEvents.DISCONNECT.register(
-        //         (handler, client) -> ClientRenderCacheHooks.clearAllInterpolationCaches());
-        // ClientLifecycleEvents.CLIENT_STOPPING.register(client -> ClientRenderCacheHooks.clearAllInterpolationCaches());
+        ClientRenderCacheHooks.setQuarryInterpolationClearer(LaserQuarryBlockEntityRenderer::clearInterpolationCache);
+        ClientRenderCacheHooks.setClearAllInterpolationCaches(LaserQuarryBlockEntityRenderer::clearAllInterpolationCaches);
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (client.level != null) {
+                LaserQuarryBlockEntityRenderer.pruneInterpolationCache(client.level);
+            }
+        });
+        ClientPlayConnectionEvents.DISCONNECT.register(
+                (handler, client) -> ClientRenderCacheHooks.clearAllInterpolationCaches());
+        ClientLifecycleEvents.CLIENT_STOPPING.register(client -> ClientRenderCacheHooks.clearAllInterpolationCaches());
     }
 }
