57ab274727a2996f6fb9c96b13f99bd086ff36a1
Kyle Klaus <kklaus@indemnity83.com>
2026-02-23 11:05:26 -0800

    Add kiln loot table and tag it as pickaxe mineable needing stone tool

--- src/main/java/com/logistics/core/fabricator/KilnBlock.java
+++ src/main/java/com/logistics/core/fabricator/KilnBlock.java
@@ -17,6 +17,8 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.material.PushReaction;
 import org.jetbrains.annotations.Nullable;
 
 /**
