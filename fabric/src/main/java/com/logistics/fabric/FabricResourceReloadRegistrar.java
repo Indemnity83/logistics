dbfb2819e05d3d0c560f374eec53c7275caacee8
Kyle Klaus <kklaus@indemnity83.com>
2026-05-06 08:44:22 -0700

    Unify platform access by implementing PlatformService and ResourceReloadRegistrar

---
+++ fabric/src/main/java/com/logistics/fabric/FabricResourceReloadRegistrar.java
@@ -0,0 +1,14 @@
+package com.logistics.fabric;
+
+import com.logistics.core.lib.platform.ResourceReloadRegistrar;
+import com.logistics.core.lib.resource.ResourceId;
+import net.fabricmc.fabric.api.resource.v1.ResourceLoader;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.resources.PreparableReloadListener;
+
+public final class FabricResourceReloadRegistrar implements ResourceReloadRegistrar {
+    @Override
+    public void register(ResourceId id, PreparableReloadListener listener) {
+        ResourceLoader.get(PackType.SERVER_DATA).registerReloadListener(id.toIdentifier(), listener);
+    }
+}
