744d139d0431971020ccb238a57e3e1ceff468f1
Kyle Klaus <kklaus@indemnity83.com>
2026-05-09 08:12:36 -0700

    refactor: introduce FuelHelper interface for fuel value management (#364)

---
+++ fabric/src/main/java/com/logistics/fabric/FabricFuelHelper.java
@@ -0,0 +1,18 @@
+package com.logistics.fabric;
+
+import com.logistics.core.lib.power.FuelHelper;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+public final class FabricFuelHelper implements FuelHelper {
+
+    @Override
+    public boolean checkIsFuel(Level level, ItemStack stack) {
+        return level.fuelValues().isFuel(stack);
+    }
+
+    @Override
+    public int checkBurnDuration(Level level, ItemStack stack) {
+        return level.fuelValues().burnDuration(stack);
+    }
+}
