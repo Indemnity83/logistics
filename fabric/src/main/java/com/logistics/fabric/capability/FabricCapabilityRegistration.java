828307ba7b1fbd539a443c2076ce4cebbe2e5eac
Kyle Klaus <kklaus@indemnity83.com>
2026-06-16 10:38:01 -0700

    feat(pipes): add fluid pipes, tanks, and powered fluid extraction (#511)
    (cherry picked from commit 16c277e8c415b299bb682db6228b34d049ca90d2)

--- fabric/src/main/java/com/logistics/fabric/capability/FabricCapabilityRegistration.java
+++ fabric/src/main/java/com/logistics/fabric/capability/FabricCapabilityRegistration.java
@@ -1,6 +1,7 @@
 package com.logistics.fabric.capability;
 
 import com.logistics.LogisticsAutomation;
+import com.logistics.core.lib.fluids.FluidContainerInteraction;
 import com.logistics.core.lib.fluids.FluidStorageLookup;
 import com.logistics.core.lib.pipe.PipeConnectionLookup;
 import com.logistics.core.lib.storage.ItemStorageLookup;
@@ -8,8 +9,10 @@ import com.logistics.fabric.fluids.FabricFluidStorage;
 import com.logistics.fabric.storage.FabricItemKey;
 import com.logistics.fabric.storage.FabricItemStorage;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidStorage;
+import net.fabricmc.fabric.api.transfer.v1.fluid.FluidStorageUtil;
 import net.fabricmc.fabric.api.transfer.v1.item.ItemStorage;
 import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionResult;
 
 public final class FabricCapabilityRegistration {
     private FabricCapabilityRegistration() {}
@@ -30,6 +33,17 @@ public final class FabricCapabilityRegistration {
         // Wire FluidStorageLookup so common code can find IFluidStorage without importing Fabric API
         FluidStorageLookup.register(
                 (world, pos, dir) -> FabricFluidStorage.wrap(FluidStorage.SIDED.find(world, pos, dir)));
+
+        // Wire bucket/fluid-container interaction (Fabric Transfer API helper) for tanks
+        FluidContainerInteraction.register((world, pos, player, hand, side) -> {
+            var storage = FluidStorage.SIDED.find(world, pos, side);
+            if (storage == null) {
+                return InteractionResult.PASS;
+            }
+            return FluidStorageUtil.interactWithFluidStorage(storage, player, hand)
+                    ? InteractionResult.SUCCESS
+                    : InteractionResult.PASS;
+        });
 
         // Quarry: only accepts pipe connections from above
         PipeConnectionRegistry.SIDED.registerForBlockEntity(
