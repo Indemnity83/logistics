e0a04f9dbe0097b7444eaab3b9385509a682d672
Kyle Klaus <kklaus@indemnity83.com>
2026-06-25 15:44:48 -0700

    refactor(quarry): split output routing from block breaking (#591)
    (cherry picked from commit 6aed86f84c3b704813c70d71cb1027e8b1137541)

--- common/src/main/java/com/logistics/automation/laserquarry/entity/LaserQuarryBlockEntity.java
+++ common/src/main/java/com/logistics/automation/laserquarry/entity/LaserQuarryBlockEntity.java
@@ -79,9 +79,13 @@ public class LaserQuarryBlockEntity extends BaseBlockEntity
     // Block breaking animation entity ID (use position hash for uniqueness).
     private final int breakingEntityId;
 
+    // Routes mined drops out (pipe/inventory above, else dropped).
+    private final QuarryOutput output;
+
     public LaserQuarryBlockEntity(BlockPos pos, BlockState state) {
         super(LogisticsAutomation.ENTITY.LASER_QUARRY_BLOCK_ENTITY, pos, state);
         this.breakingEntityId = pos.hashCode();
+        this.output = new QuarryOutput(pos);
     }
 
     // ==================== HasEnergyStorage ====================
@@ -180,6 +184,11 @@ public class LaserQuarryBlockEntity extends BaseBlockEntity
     @Override
     public ArmController arm() {
         return armController;
+    }
+
+    @Override
+    public QuarryOutput output() {
+        return output;
     }
 
     @Override
