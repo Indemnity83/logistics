f3c4db8b863cccf3e77a2545d09925af2a47e732
Kyle Klaus <kklaus@indemnity83.com>
2026-06-30 22:09:16 -0700

    feat(worldgen): add bog earth and peat fuel (#658)
    (cherry picked from commit f6b9b17bf83ed83876ab769cf9203dcba65f07f4)

--- fabric/src/main/java/com/logistics/fabric/LogisticsFabric.java
+++ fabric/src/main/java/com/logistics/fabric/LogisticsFabric.java
@@ -1,5 +1,6 @@
 package com.logistics.fabric;
 
+import com.logistics.LogisticsCore;
 import com.logistics.LogisticsMod;
 import com.logistics.LogisticsPower;
 import com.logistics.core.bootstrap.LogisticsCommonBootstrap;
@@ -11,6 +12,7 @@ import com.logistics.core.lib.power.AbstractEngineBlock;
 import com.logistics.fabric.capability.FabricCapabilityRegistration;
 import com.logistics.fabric.energy.EnergyStorageAccess;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.registry.FuelValueEvents;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
@@ -35,6 +37,9 @@ public final class LogisticsFabric implements ModInitializer {
         FabricPlayerJoinEvents.register();
         FabricPacketRegistration.register();
         FabricBiomeModifications.register();
+
+        // Peat burns long enough to smelt 10 items; works in furnaces and the Stirling Engine.
+        FuelValueEvents.BUILD.register((builder, context) -> builder.add(LogisticsCore.ITEM.PEAT, 2000));
 
         FabricLoader.getInstance().getModContainer(LogisticsMod.MOD_ID).ifPresent(container ->
             ResourceManagerHelper.registerBuiltinResourcePack(
