01ec5eeb5af70fc95f3b9af9b1c76ac68195b4b6
Kyle Klaus <kklaus@indemnity83.com>
2026-08-03 22:05:34 -0700

    refactor(fluids): add internal fluid packet item and component (#789)
    (cherry picked from commit cbabf09a8d3a3367916476cddaacfb22bdb6d24d)

--- neoforge/src/main/java/com/logistics/neoforge/client/NeoForgeClientSetup.java
+++ neoforge/src/main/java/com/logistics/neoforge/client/NeoForgeClientSetup.java
@@ -31,6 +31,8 @@ import com.logistics.pipe.screen.RequesterScreen;
 import com.logistics.pipe.screen.SatelliteScreen;
 import com.logistics.pipe.screen.SinkScreen;
 import com.logistics.pipe.screen.SupplierScreen;
+import com.logistics.pipe.client.FluidPacketRendering;
+import com.logistics.pipe.client.FluidPacketSpecialRenderer;
 import com.logistics.automation.render.LaserQuarryBlockEntityRenderer;
 import com.logistics.core.render.MarkerBlockEntityRenderer;
 import com.logistics.pipe.network.packet.SyncRequesterInventoryPacket;
@@ -58,6 +60,7 @@ import net.neoforged.neoforge.client.network.event.RegisterClientPayloadHandlers
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
+import net.neoforged.neoforge.client.event.RegisterSpecialModelRendererEvent;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.neoforge.client.network.ClientPacketDistributor;
 import net.neoforged.neoforge.client.event.ClientPlayerNetworkEvent;
@@ -70,6 +73,7 @@ public final class NeoForgeClientSetup {
         modBus.addListener(NeoForgeClientSetup::onClientSetup);
         modBus.addListener(NeoForgeClientSetup::registerScreens);
         modBus.addListener(NeoForgeClientSetup::registerRenderers);
+        modBus.addListener(NeoForgeClientSetup::registerSpecialModelRenderers);
         modBus.addListener(NeoForgeClientSetup::registerClientPayloadHandlers);
         modBus.addListener(NeoForgeClientSetup::registerFluidExtensions);
         NeoForge.EVENT_BUS.addListener(NeoForgeClientSetup::onClientDisconnect);
@@ -209,6 +213,10 @@ public final class NeoForgeClientSetup {
         event.registerBlockEntityRenderer(
                 LogisticsAutomation.ENTITY.REFINERY_BLOCK_ENTITY,
                 com.logistics.automation.refinery.RefineryBlockEntityRenderer::new);
+    }
+
+    private static void registerSpecialModelRenderers(RegisterSpecialModelRendererEvent event) {
+        event.register(FluidPacketRendering.ID.toIdentifier(), FluidPacketSpecialRenderer.Unbaked.MAP_CODEC);
     }
 
     private static void registerClientPayloadHandlers(RegisterClientPayloadHandlersEvent event) {
