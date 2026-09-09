8a8839f47e0745b62de3147d1efb07e212e11290
Kyle Klaus <kklaus@indemnity83.com>
2026-09-09 10:26:37 -0700

    test(quarry): prove power, pipe and item contracts through real blocks

--- neoforge/src/gametest/java/com/logistics/gametest/automation/QuarryMiningGameTestRegistration.java
+++ neoforge/src/gametest/java/com/logistics/gametest/automation/QuarryMiningGameTestRegistration.java
@@ -45,6 +45,8 @@ public final class QuarryMiningGameTestRegistration {
         new GameTestCase(
             "automation/quarry_mines_and_outputs_via_real_engine", 220, QuarryMiningGameTestBody::testQuarryMinesAndOutputsViaRealEngine),
         new GameTestCase(
+            "automation/quarry_mines_waterlogged_block", 220, QuarryMiningGameTestBody::testQuarryMinesWaterloggedBlock),
+        new GameTestCase(
             "automation/quarry_treats_lava_as_unminable_and_skips_that_column",
             220,
             QuarryMiningGameTestBody::testQuarryTreatsLavaAsUnminableAndSkipsThatColumn),
