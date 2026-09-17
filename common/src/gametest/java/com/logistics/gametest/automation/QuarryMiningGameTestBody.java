ac760a58fa2700675aa77e350543c74bc5afd3c8
Indemnity83 <kklaus@indemnity83.com>
2026-09-16 18:57:06 -0700

    Restore the Collections import CableNetwork needs

--- common/src/gametest/java/com/logistics/gametest/automation/QuarryMiningGameTestBody.java
+++ common/src/gametest/java/com/logistics/gametest/automation/QuarryMiningGameTestBody.java
@@ -23,7 +23,6 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.ChestBlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.storage.TagValueInput;
 
