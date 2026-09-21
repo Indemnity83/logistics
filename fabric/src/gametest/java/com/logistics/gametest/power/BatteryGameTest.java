486f1c9caa25940f6445d1bcb9a6f9ce3e764560
Kyle Klaus <kklaus@indemnity83.com>
2026-09-21 16:04:03 -0700

    test(energy): pin how a cable splits power across a battery bank (#1252)
    (cherry picked from commit 457913ec9a8d3f4dcd0ed54c0c5bdac3b1192391)

--- fabric/src/gametest/java/com/logistics/gametest/power/BatteryGameTest.java
+++ fabric/src/gametest/java/com/logistics/gametest/power/BatteryGameTest.java
@@ -50,4 +50,10 @@ public class BatteryGameTest {
     public void testBatteriesOnOneCableDoNotDrainEachOther(GameTestHelper context) {
         BatteryGameTestBody.testBatteriesOnOneCableDoNotDrainEachOther(context);
     }
+
+    /** A bank of batteries on one cable charges together rather than one at a time. */
+    @GameTest(maxTicks = 60)
+    public void testBatteriesOnOneCableFillEvenly(GameTestHelper context) {
+        BatteryGameTestBody.testBatteriesOnOneCableFillEvenly(context);
+    }
 }
